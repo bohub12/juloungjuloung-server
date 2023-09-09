be6e0a48734fed58992b2a937f5b50e474f948d4
bohub12 <qhrb1997@naver.com>
2023-09-09 16:56:20 +0900

    Chore: ktlint 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,73 +1,73 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.1.3"
-	id("io.spring.dependency-management") version "1.1.3" apply false
-	kotlin("jvm") version "1.8.22"
-	kotlin("plugin.spring") version "1.8.22" apply false
-	kotlin("plugin.jpa") version "1.8.22" apply false
+    id("org.springframework.boot") version "3.1.3"
+    id("io.spring.dependency-management") version "1.1.3" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "11.3.1"
+    kotlin("jvm") version "1.8.22"
+    kotlin("plugin.spring") version "1.8.22" apply false
+    kotlin("plugin.jpa") version "1.8.22" apply false
 }
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 allprojects {
-	group = "com.juloungjuloung"
-	version = "0.0.1-SNAPSHOT"
-	repositories {
-		mavenCentral()
-	}
+    group = "com.juloungjuloung"
+    version = "0.0.1-SNAPSHOT"
+    repositories {
+        mavenCentral()
+    }
 
-	tasks.withType<KotlinCompile> {
-		kotlinOptions {
-			freeCompilerArgs += "-Xjsr305=strict"
-			jvmTarget = "17"
-		}
-	}
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs += "-Xjsr305=strict"
+            jvmTarget = "17"
+        }
+    }
 
-	tasks.withType<Test> {
-		useJUnitPlatform()
-	}
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "17"
-	}
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 subprojects {
-	apply(plugin = "org.jetbrains.kotlin.jvm")
-	apply(plugin = "org.jetbrains.kotlin.plugin.spring")
-	apply(plugin = "org.springframework.boot")
-	apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
 
-	dependencies {
-		implementation("org.springframework.boot:spring-boot-starter-actuator")
-		implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-		implementation("org.springframework.boot:spring-boot-starter-web")
-		implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-		implementation("org.jetbrains.kotlin:kotlin-reflect")
-		compileOnly("org.projectlombok:lombok")
-		developmentOnly("org.springframework.boot:spring-boot-devtools")
-		runtimeOnly("com.h2database:h2")
-		runtimeOnly("com.mysql:mysql-connector-j")
-		runtimeOnly("io.micrometer:micrometer-registry-prometheus")
-		runtimeOnly("org.postgresql:postgresql")
-		annotationProcessor("org.projectlombok:lombok")
-		testImplementation("org.springframework.boot:spring-boot-starter-test")
-	}
+    dependencies {
+        implementation("org.springframework.boot:spring-boot-starter-actuator")
+        implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+        implementation("org.springframework.boot:spring-boot-starter-web")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        developmentOnly("org.springframework.boot:spring-boot-devtools")
+        runtimeOnly("com.h2database:h2")
+        runtimeOnly("com.mysql:mysql-connector-j")
+        runtimeOnly("io.micrometer:micrometer-registry-prometheus")
+        runtimeOnly("org.postgresql:postgresql")
+        annotationProcessor("org.projectlombok:lombok")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+    }
 }
-
