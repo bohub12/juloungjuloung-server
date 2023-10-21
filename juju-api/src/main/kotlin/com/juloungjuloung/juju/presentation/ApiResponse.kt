fdcd5c78ebcb75098719c8eb9e6f93c5f4029a3a
bohub12 <qhrb1997@naver.com>
2023-10-22 01:12:10 +0900

    Feat: 공통 Response 형식

---
+++ juju-api/src/main/kotlin/com/juloungjuloung/juju/presentation/ApiResponse.kt
@@ -0,0 +1,46 @@
+package com.juloungjuloung.juju.presentation
+
+import com.fasterxml.jackson.annotation.JsonInclude
+
+data class ApiResponse<T>(
+    val code: Int,
+    val message: String,
+    val data: T?,
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    val pageResponse: PageResponse?
+) {
+    companion object {
+
+        fun <T> success(data: T) = ApiResponse(
+            code = ApiResponseCode.SUCCESS.code,
+            message = ApiResponseCode.SUCCESS.message,
+            data = data,
+            pageResponse = null
+        )
+
+        fun <T> success(data: T, pageResponse: PageResponse?): ApiResponse<T> {
+            return ApiResponse(
+                code = ApiResponseCode.SUCCESS.code,
+                message = ApiResponseCode.SUCCESS.message,
+                data = data,
+                pageResponse = pageResponse
+            )
+        }
+
+        fun <T> fail(responseCode: ApiResponseCode): ApiResponse<T> {
+            return ApiResponse(
+                code = responseCode.code,
+                message = responseCode.message,
+                data = null,
+                pageResponse = null
+            )
+        }
+    }
+}
+
+data class PageResponse(
+    val page: Int,
+    val size: Int,
+    val totalPageCount: Int
+)
