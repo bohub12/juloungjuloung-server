afdb0eff680426dcc12804797f90471255c719ca
bohub12 <qhrb1997@naver.com>
2023-11-23 01:43:41 +0900

    Fix: when 구문 수정

--- juju-core/application/src/test/kotlin/com/juloungjuloung/juju/service/factory/ProductServiceFactoryTest.kt
+++ juju-core/application/src/test/kotlin/com/juloungjuloung/juju/service/factory/ProductServiceFactoryTest.kt
@@ -25,7 +25,7 @@ class ProductServiceFactoryTest : BehaviorSpec({
     val productServiceFactory = ProductServiceFactory(productServices)
 
     Given("Product Service들이 팩토리에 등록됐을 때") {
-        When("Enum 타입으로 Service를 조회하면") {
+        When("정상적인 파라미터(enum)로 Service를 조회하면") {
             Then("정상적으로 리턴한다") {
                 productServiceFactory.get(ProductTypeEnum.BRACELET).shouldBeTypeOf<BraceletService>()
                 productServiceFactory.get(ProductTypeEnum.EARRING).shouldBeTypeOf<EarringService>()
