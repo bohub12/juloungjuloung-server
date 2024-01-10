df961df99b2041bd508a08c868317f60c8bb802b
bohub12 <qhrb1997@naver.com>
2024-01-11 01:26:09 +0900

    Test: ProductImageServiceFacade 저장 로직 테스트코드

--- juju-core/src/test/kotlin/com/juloungjuloung/juju/application/facade/product/ProductServiceFacadeTest.kt
+++ juju-core/src/test/kotlin/com/juloungjuloung/juju/application/facade/product/ProductServiceFacadeTest.kt
@@ -2,7 +2,7 @@ package com.juloungjuloung.juju.application.facade.product
 
 import com.juloungjuloung.juju.application.factory.ProductServiceFactory
 import com.juloungjuloung.juju.domain.product.service.impl.RingService
-import com.juloungjuloung.juju.domain.saveRingVO
+import com.juloungjuloung.juju.domain.saveRingVOFixture
 import com.juloungjuloung.juju.enums.ProductTypeEnum
 import io.kotest.core.spec.style.BehaviorSpec
 import io.mockk.every
@@ -21,7 +21,7 @@ class ProductServiceFacadeTest : BehaviorSpec({
             every { productServiceFactory.get(any()) } returns ringService
 
             productServiceFacade.readProducts(ProductTypeEnum.RING, 0, 10)
-            productServiceFacade.saveProduct(saveRingVO())
+            productServiceFacade.saveProduct(saveRingVOFixture())
 
             Then("해당 서비스에게 요청을 위임한다") {
                 verify { ringService.read(any(), any()) }
--- juju-core/src/testFixtures/kotlin/com/juloungjuloung/juju/domain/VOTestFixtures.kt
+++ juju-core/src/testFixtures/kotlin/com/juloungjuloung/juju/domain/VOTestFixtures.kt
@@ -1,5 +1,7 @@
 package com.juloungjuloung.juju.domain
 
+import com.juloungjuloung.juju.domain.product.vo.SaveProductImageInternalVO
+import com.juloungjuloung.juju.domain.product.vo.SaveProductImageVO
 import com.juloungjuloung.juju.domain.product.vo.SaveProductVO
 import com.juloungjuloung.juju.enums.ProductTypeEnum
 import com.juloungjuloung.juju.fixtureMonkey
@@ -7,7 +9,7 @@ import com.navercorp.fixturemonkey.kotlin.giveMeBuilder
 import com.navercorp.fixturemonkey.kotlin.setExp
 import com.navercorp.fixturemonkey.kotlin.setNotNullExp
 
-fun saveRingVO(): SaveProductVO {
+fun saveRingVOFixture(): SaveProductVO {
     return fixtureMonkey.giveMeBuilder<SaveProductVO>()
         .setExp(SaveProductVO::productType, ProductTypeEnum.RING)
         .setNotNullExp(SaveProductVO::name)
@@ -17,4 +19,45 @@ fun saveRingVO(): SaveProductVO {
         .setNotNullExp(SaveProductVO::thumbnailImage)
         .setExp(SaveProductVO::isDiamond, false)
         .sample()
+}
+
+fun saveProductImageVOFixture(
+    isMultiplePrimaryImage: Boolean = false,
+    exceedMaxSize: Boolean = false
+): SaveProductImageVO {
+    return fixtureMonkey.giveMeBuilder<SaveProductImageVO>()
+        .setExp(SaveProductImageVO::productId, 1L)
+        .setExp(
+            SaveProductImageVO::saveProductImageInternalVOs,
+            saveProductImageInternalVOFixture(isMultiplePrimaryImage, exceedMaxSize)
+        )
+        .sample()
+}
+
+private fun saveProductImageInternalVOFixture(
+    isMultiplePrimaryImage: Boolean = false,
+    exceedMaxSize: Boolean = false
+): List<SaveProductImageInternalVO> {
+    val dummyPrimaryImageVO = SaveProductImageInternalVO("primary.png", true)
+    val dummyNormalImageVO = SaveProductImageInternalVO("normal.png", false)
+
+    return if (!isMultiplePrimaryImage && !exceedMaxSize) {
+        listOf(dummyPrimaryImageVO, dummyNormalImageVO)
+    } else if (isMultiplePrimaryImage) {
+        listOf(dummyPrimaryImageVO, dummyPrimaryImageVO)
+    } else {
+        listOf(
+            dummyPrimaryImageVO,
+            dummyNormalImageVO,
+            dummyNormalImageVO,
+            dummyNormalImageVO,
+            dummyNormalImageVO,
+            dummyNormalImageVO,
+            dummyNormalImageVO,
+            dummyNormalImageVO,
+            dummyNormalImageVO,
+            dummyNormalImageVO,
+            dummyNormalImageVO
+        )
+    }
 }
