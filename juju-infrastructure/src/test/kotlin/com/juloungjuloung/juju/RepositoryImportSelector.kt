029ab31f0e4c4a95435f14fe16c92ab0a85effe4
bohub12 <qhrb1997@naver.com>
2024-01-22 00:27:22 +0900

    Fix: Repository 통합테스트 어노테이션 수정 (여러 리포지토리 주입 가능하게)

--- juju-infrastructure/src/test/kotlin/com/juloungjuloung/juju/RepositoryImportSelector.kt
+++ juju-infrastructure/src/test/kotlin/com/juloungjuloung/juju/RepositoryImportSelector.kt
@@ -4,12 +4,11 @@ import org.springframework.context.annotation.ImportSelector
 import org.springframework.core.type.AnnotationMetadata
 
 class RepositoryImportSelector : ImportSelector {
-
     override fun selectImports(importingClassMetadata: AnnotationMetadata): Array<String> {
-        val repositoryClass = importingClassMetadata.getAnnotationAttributes(
+        val repositoryClasses = importingClassMetadata.getAnnotationAttributes(
             RepositoryIntegrationTest::class.qualifiedName!!
-        )?.get("value") as Class<*>
+        )?.get("value") as Array<Class<*>>
 
-        return arrayOf(repositoryClass.name)
+        return repositoryClasses.map { it.name }.toTypedArray()
     }
 }
