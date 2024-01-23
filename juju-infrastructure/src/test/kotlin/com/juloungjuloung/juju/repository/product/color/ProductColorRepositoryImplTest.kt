8de3d929a3934e25f1bc45bf0050c8af50453905
bohub12 <qhrb1997@naver.com>
2024-01-24 02:40:40 +0900

    Test: 상품 색상 조회 & 저장 리포지토리 슬라이스 테스트

--- juju-infrastructure/src/test/kotlin/com/juloungjuloung/juju/repository/product/color/ProductColorRepositoryImplTest.kt
+++ juju-infrastructure/src/test/kotlin/com/juloungjuloung/juju/repository/product/color/ProductColorRepositoryImplTest.kt
@@ -2,6 +2,8 @@ package com.juloungjuloung.juju.repository.product.color
 
 import com.juloungjuloung.juju.RepositoryIntegrationTest
 import com.juloungjuloung.juju.domain.productcolor.productColorCollectionFixture
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
+import io.kotest.matchers.shouldBe
 import jakarta.persistence.EntityManager
 import jakarta.persistence.PersistenceContext
 import org.junit.jupiter.api.AfterEach
@@ -24,15 +26,32 @@ class ProductColorRepositoryImplTest {
     }
 
     @Test
+    fun `findById_성공`() {
+        // given
+        val productId = 1L
+        val productColorIds = productColorRepositoryImpl.saveAll(productColorCollectionFixture(productId = productId))
+
+        // when
+        val productColors = productColorRepositoryImpl.findByProduct(productId)
+
+        // then
+        productColors.size shouldBe productColorIds.size
+        productColors.map { it.id } shouldContainExactlyInAnyOrder productColorIds
+    }
+
+    @Test
     fun `saveAll_성공`() {
         // given
         val productId = 1L
         val productColors = productColorCollectionFixture(productId = productId)
 
         // when
-        productColorRepositoryImpl.saveAll(productColors)
+        val savedProductColorIds = productColorRepositoryImpl.saveAll(productColors)
 
         // then
-        // TODO : findByProduct 로 조회해서 저장되었는지 확인하는 로직 추가
+        val findProductColors = productColorRepositoryImpl.findByProduct(productId)
+        findProductColors.size shouldBe savedProductColorIds.size
+        findProductColors.map { it.color } shouldContainExactlyInAnyOrder productColors.map { it.color }
+        findProductColors.map { it.additionalPrice } shouldContainExactlyInAnyOrder productColors.map { it.additionalPrice }
     }
 }
