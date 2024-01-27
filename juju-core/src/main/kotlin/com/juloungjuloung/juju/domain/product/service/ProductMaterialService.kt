083c27cac156e6b8290170a0926281c24c61b7c9
bohub12 <qhrb1997@naver.com>
2024-01-27 14:13:04 +0900

    Feat: 상품 재질 저장 비즈니스 로직 구현

---
+++ juju-core/src/main/kotlin/com/juloungjuloung/juju/domain/product/service/ProductMaterialService.kt
@@ -0,0 +1,36 @@
+package com.juloungjuloung.juju.domain.product.service
+
+import com.juloungjuloung.juju.domain.product.Product
+import com.juloungjuloung.juju.domain.product.combineForValidation
+import com.juloungjuloung.juju.domain.product.repository.ProductMaterialRepository
+import com.juloungjuloung.juju.domain.product.repository.ProductRepository
+import com.juloungjuloung.juju.domain.product.vo.SaveProductMaterialVO
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class ProductMaterialService(
+    private val productMaterialRepository: ProductMaterialRepository,
+    private val productRepository: ProductRepository
+) {
+
+    @Transactional
+    fun saveAll(saveProductMaterialVO: SaveProductMaterialVO): List<Long> {
+        validateSaveCondition(saveProductMaterialVO)
+
+        return productMaterialRepository.saveAll(saveProductMaterialVO.toDomain())
+    }
+
+    private fun validateSaveCondition(saveProductMaterialVO: SaveProductMaterialVO) {
+        findProductOrException(saveProductMaterialVO.productId)
+
+        val productMaterials = saveProductMaterialVO.toDomain()
+        val savedProductMaterials = productMaterialRepository.findByProduct(saveProductMaterialVO.productId)
+
+        savedProductMaterials.combineForValidation(productMaterials)
+    }
+
+    private fun findProductOrException(productId: Long): Product {
+        return productRepository.findById(productId)
+    }
+}
