f6e6fc189a80aa74944529e83eb464bdfa6f0eb8
bohub12 <qhrb1997@naver.com>
2024-02-20 15:56:29 +0900

    Fix: cors setting (임시로 local 환경 프론트에서 접근할 수 있도록 수정)

--- juju-admin/src/main/kotlin/com/juloungjuloung/juju/config/WebConfig.kt
+++ juju-admin/src/main/kotlin/com/juloungjuloung/juju/config/WebConfig.kt
@@ -3,6 +3,7 @@ package com.juloungjuloung.juju.config
 import com.juloungjuloung.juju.config.typeconverter.StringToEnumConverterFactory
 import org.springframework.context.annotation.Configuration
 import org.springframework.format.FormatterRegistry
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
@@ -10,5 +11,10 @@ class WebConfig : WebMvcConfigurer {
 
     override fun addFormatters(registry: FormatterRegistry) {
         registry.addConverterFactory(StringToEnumConverterFactory())
+    }
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
     }
 }
