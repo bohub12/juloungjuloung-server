a169a84de7665740eb66e272a8b0bf59afd58d88
bohub12 <qhrb1997@naver.com>
2024-04-09 20:35:04 +0900

    Fix: logback-spring.xml
    - 로그 레벨 Threshold를 Error 로 설정
    - 스프링프로필별 appender 설정

--- juju-support/logging/admin-logging/build.gradle.kts
+++ juju-support/logging/admin-logging/build.gradle.kts
@@ -3,7 +3,4 @@ dependencies {
 
     // RestTemplate
     implementation("org.springframework:spring-web")
-
-    // ConfigurationProperties
-    implementation("org.springframework.boot:spring-boot-autoconfigure")
 }
--- juju-support/logging/admin-logging/src/main/kotlin/com/juloungjuloung/juju/appender/SlackAppender.kt
+++ juju-support/logging/admin-logging/src/main/kotlin/com/juloungjuloung/juju/appender/SlackAppender.kt
@@ -2,22 +2,22 @@ package com.juloungjuloung.juju.appender
 
 import ch.qos.logback.classic.spi.ILoggingEvent
 import ch.qos.logback.classic.spi.IThrowableProxy
-import ch.qos.logback.core.AppenderBase
+import ch.qos.logback.core.UnsynchronizedAppenderBase
 import org.springframework.web.client.RestTemplate
 import java.text.SimpleDateFormat
 
-class SlackAppender(
-    private val slackProperties: SlackProperties
-) : AppenderBase<ILoggingEvent>() {
+class SlackAppender : UnsynchronizedAppenderBase<ILoggingEvent>() {
     companion object {
         private const val STACK_TRACE_ELEMENT_MAX_SIZE = 15
     }
+
+    lateinit var webHookUrl: String
 
     override fun append(eventObject: ILoggingEvent?) {
         val restTemplate = RestTemplate()
 
         val body: Map<String, Any> = createSlackMessage(eventObject!!)
-        restTemplate.postForEntity(slackProperties.webHookUrl, body, String::class.java)
+        restTemplate.postForEntity(webHookUrl, body, String::class.java)
     }
 
     private fun createSlackMessage(eventObject: ILoggingEvent): Map<String, Any> {
@@ -30,18 +30,6 @@ class SlackAppender(
                     "pretext" to "에러가 발생했어요. 아래 로그를 확인해주세요 :cry:",
                     "author_name" to "어드민서버-에러로그",
                     "text" to message,
-                    "fields" to listOf(
-                        mapOf(
-                            "title" to "우선순위",
-                            "value" to "High",
-                            "short" to false
-                        ),
-                        mapOf(
-                            "title" to "서버 환경",
-                            "value" to "local",
-                            "short" to false
-                        )
-                    ),
                     "ts" to eventObject.timeStamp
                 )
             )
