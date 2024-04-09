b8c5701c6a97798d850e307fe6231270729dc6f9
bohub12 <qhrb1997@naver.com>
2024-04-09 20:32:11 +0900

    Feat: logback-spring.xml 파일에서 암호화된 springProperty 읽을 수 있게 environment 지정

--- juju-admin/src/main/kotlin/com/juloungjuloung/juju/JujuAdminApiApplication.kt
+++ juju-admin/src/main/kotlin/com/juloungjuloung/juju/JujuAdminApiApplication.kt
@@ -1,11 +1,17 @@
 package com.juloungjuloung.juju
 
+import com.juloungjuloung.juju.config.CustomEncryptedPropertyDetector
+import com.ulisesbocchio.jasyptspringboot.environment.StandardEncryptableEnvironment
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 
 @SpringBootApplication
 class JujuAdminApiApplication
 
 fun main(args: Array<String>) {
-    runApplication<JujuAdminApiApplication>(*args)
+    // This environment is for springProperty in logback-spring.xml
+    SpringApplicationBuilder()
+        .environment(StandardEncryptableEnvironment.builder().detector(CustomEncryptedPropertyDetector()).build())
+        .sources(JujuAdminApiApplication::class.java)
+        .run(*args)
 }
